fe2196452f0aee7a42505df6ee89f86df4e6bdb4
HafizhHabiibi <hafizhhabiibi123@gmail.com>
2024-12-04 20:30:10 +0700

    add model

---
+++ app/Models/Address.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'order_id',
+        'first_name',
+        'last_name',
+        'phone',
+        'alamat',
+        'kabkota',
+        'provinsi',
+        'kode_pos'
+    ];
+
+    public function order() {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function gerFullNameAttribute() {
+        return "{$this->first_name} {$this->last_name}"; 
+    }
+
+}
---
+++ app/Models/Product.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'name',
+        'slug',
+        'images',
+        'description',
+        'price',
+        'is_active',
+        'is_featured',
+        'in_stok',
+        'on_sale',
+    ];
+
+    protected $casts = [
+        'images' => 'array',
+    ];
+
+    public function category() {
+        return $this->belongsTo((Category::class));
+    }
+
+    public function orderitems() {
+        return $this->hasMany(OrderItem::class); 
+    }
+
+}

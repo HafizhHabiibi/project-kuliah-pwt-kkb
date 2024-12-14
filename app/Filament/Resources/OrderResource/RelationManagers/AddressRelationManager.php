a19dd7bf798a8a25b38986a1f991ff4117ca850b
HafizhHabiibi <hafizhhabiibi123@gmail.com>
2024-12-14 21:49:20 +0700

    tambah kolom alamat

---
+++ app/Filament/Resources/OrderResource/RelationManagers/AddressRelationManager.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Filament\Resources\OrderResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\TextArea;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class AddressRelationManager extends RelationManager
+{
+    protected static string $relationship = 'address';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+
+                TextInput::make('first_name')
+                ->required()
+                ->maxLength(255),
+
+                TextInput::make('last_name')
+                ->required()
+                ->maxLength(255),
+
+                TextInput::make('phone')
+                ->required()
+                ->tel()
+                ->maxLength(20),
+
+                TextInput::make('kabkota')
+                ->required()
+                ->maxLength(255),
+
+                TextInput::make('provinsi')
+                ->required()
+                ->maxLength(255),
+
+                TextInput::make('kode_pos')
+                ->required()
+                ->maxLength(255),
+
+                TextArea::make('alamat')
+                    ->required()
+                    ->columnSpanFull(),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('alamat')
+            ->columns([
+                TextColumn::make('fullname')
+                ->label('Nama Lengkap'),
+
+                TextColumn::make('phone'),
+
+                TextColumn::make('kabkota'),
+
+                TextColumn::make('provinsi'),
+
+                TextColumn::make('kode_pos'),
+
+                TextColumn::make('alamat'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}

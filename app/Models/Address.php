17e0f2364af34905f3edb380a95f2737a6b29aad
HafizhHabiibi <hafizhhabiibi123@gmail.com>
2024-12-14 21:49:30 +0700

    alamat

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -23,7 +23,7 @@ class Address extends Model
         return $this->belongsTo(Order::class);
     }
 
-    public function gerFullNameAttribute() {
+    public function getFullNameAttribute() {
         return "{$this->first_name} {$this->last_name}"; 
     }
 

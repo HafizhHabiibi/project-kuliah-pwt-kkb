c2e35e71ba2a5d65bf6d759e8c04a59e3655d9ae
HafizhHabiibi <hafizhhabiibi123@gmail.com>
2024-12-19 21:08:54 +0700

    Rapikan dan ubah border homepage

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -49,32 +49,17 @@
   <!-- End Slider -->
 
   <!-- Kategori -->
-  <div class="bg-orange-200 py-20">
+  <div class="bg-white py-20">
     <div class="max-w-xl mx-auto">
-      <div class="text-center ">
+      <div class="text-center">
         <div class="relative flex flex-col items-center">
-          <h1 class="text-5xl font-bold dark:text-gray-200"> Browse <span class="text-blue-500"> Categories
-            </span> </h1>
-          <div class="flex w-40 mt-2 mb-6 overflow-hidden rounded">
-            <div class="flex-1 h-2 bg-blue-200">
-            </div>
-            <div class="flex-1 h-2 bg-blue-400">
-            </div>
-            <div class="flex-1 h-2 bg-blue-600">
-            </div>
-          </div>
+          <h1 class="text-5xl font-bold dark:text-gray-200">Kategori Produk</h1>
         </div>
-        <p class="mb-12 text-base text-center text-gray-500">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus magni eius eaque?
-          Pariatur
-          numquam, odio quod nobis ipsum ex cupiditate?
-        </p>
       </div>
     </div>
 
     <div class="max-w-[85rem] px-4 sm:px-6 lg:px-8 mx-auto">
       <div class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 sm:gap-6">
-
         @foreach ($categories as $category)
         <a class="group flex flex-col bg-white border shadow-sm rounded-xl hover:shadow-md transition dark:bg-slate-900 dark:border-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#" wire:key="{{ $category->id }}">
           <div class="p-4 md:p-5">
@@ -96,13 +81,9 @@
           </div>
         </a>
         @endforeach
-        
-
-        <!-- Other category items go here -->
-
       </div>
     </div>
-
   </div>
   <!-- Kategori End -->
+
 </div>

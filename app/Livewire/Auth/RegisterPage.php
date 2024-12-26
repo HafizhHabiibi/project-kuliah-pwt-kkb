d07e9182b1bbef466771a35ee703fec48536a08b
HafizhHabiibi <hafizhhabiibi123@gmail.com>
2024-12-26 10:38:49 +0700

    fungsionalitas register

--- app/Livewire/Auth/RegisterPage.php
+++ app/Livewire/Auth/RegisterPage.php
@@ -2,10 +2,44 @@
 
 namespace App\Livewire\Auth;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Registrasi')]
 class RegisterPage extends Component
 {
+
+    public $name;
+    public $email;
+    public $password;
+
+    // registrasi user
+    public function save() {
+        $this->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|min:6|max:255',
+        ]);
+
+    // save data user ke database
+        $user = User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => Hash::make($this->password),
+        ]);
+
+    // login user
+        auth()->login($user);
+
+    // redirect ke home
+        return redirect()->intended();
+        
+    }
+
+    
+
     public function render()
     {
         return view('livewire.auth.register-page');

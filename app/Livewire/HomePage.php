ac1d03b09ffa443300cd2555e3ac557c53bca809
HafizhHabiibi <hafizhhabiibi123@gmail.com>
2024-12-29 09:15:43 +0700

    tambah route untuk slider di homepage

--- app/Livewire/HomePage.php
+++ app/Livewire/HomePage.php
@@ -3,18 +3,21 @@
 namespace App\Livewire;
 
 use App\Models\Category;
+use App\Models\Slider; // Tambahkan import model Slider
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
-#[Title(('Home Page - Bharata'))]
+#[Title('Home Page - Bharata')]
 class HomePage extends Component
 {
     public function render()
     {
         $categories = Category::where('is_active', 1)->get();
+        $sliders = Slider::all(); // Ambil data slider yang aktif
 
         return view('livewire.home-page', [
-            'categories' => $categories
+            'categories' => $categories,
+            'sliders' => $sliders, // Kirim data slider ke view
         ]);
     }
 }

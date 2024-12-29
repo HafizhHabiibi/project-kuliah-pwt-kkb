5403bca3e7de540e07678cff63513491a4b7db8a
HafizhHabiibi <hafizhhabiibi123@gmail.com>
2024-12-29 19:49:46 +0700

    fungsionalitas halaman checkout

--- app/Livewire/CheckoutPage.php
+++ app/Livewire/CheckoutPage.php
@@ -2,12 +2,45 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartManagement;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Checkout-Bharata')]
 class CheckoutPage extends Component
 {
+
+    public $first_name;
+    public $last_name;
+    public $phone;
+    public $alamat;
+    public $kabkota;
+    public $provinsi;
+    public $kode_pos;
+    public $payment_method;
+
+    public function placeorder() {
+        $this->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'phone' => 'required',
+            'alamat' => 'required',
+            'kabkota' => 'required',
+            'provinsi' => 'required',
+            'kode_pos' => 'required',
+            'payment_method' => 'required',
+        ]);
+    }
+
+
+
     public function render()
     {
-        return view('livewire.checkout-page');
+        $cart_items = CartManagement::getCartItemsFromCookie();
+        $grand_total = CartManagement::calculateGrandTotal($cart_items);
+        return view('livewire.checkout-page', [
+            'cart_items' => $cart_items,
+            'grand_total' => $grand_total,
+        ]);
     }
 }

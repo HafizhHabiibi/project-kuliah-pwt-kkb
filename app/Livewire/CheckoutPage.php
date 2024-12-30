302074e123dd567b824d8653a22a95cc20b8be37
HafizhHabiibi <hafizhhabiibi123@gmail.com>
2024-12-30 10:38:26 +0700

    fungsionalitas checkout

--- app/Livewire/CheckoutPage.php
+++ app/Livewire/CheckoutPage.php
@@ -3,6 +3,8 @@
 namespace App\Livewire;
 
 use App\Helpers\CartManagement;
+use App\Models\Address;
+use App\Models\Order;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
@@ -19,6 +21,13 @@ class CheckoutPage extends Component
     public $kode_pos;
     public $payment_method;
 
+    public function mount() {
+        $cart_items = CartManagement::getCartItemsFromCookie();
+        if (count($cart_items) == 0) {
+            return redirect('/products');
+        }
+    }
+
     public function placeorder() {
         $this->validate([
             'first_name' => 'required',
@@ -30,9 +39,51 @@ class CheckoutPage extends Component
             'kode_pos' => 'required',
             'payment_method' => 'required',
         ]);
+
+        $cart_items = CartManagement::getCartItemsFromCookie();
+
+        $line_items = [];
+
+        foreach ($cart_items as $item) {
+            $line_items[] = [
+                'price_data' => [
+                    'currency' => 'idr',
+                    'unit_amount' => $item['unit_amount'] * 100,
+                    'product_data' => [
+                        'name' => $item['name'],
+                    ]
+                ],
+                'quantity' => $item['quantity'],
+            ];
+        }
+
+        $order = new Order();
+        $order->user_id = auth()->user()->id;
+        $order->grand_total = CartManagement::calculateGrandTotal($cart_items);
+        $order->payment_method = $this->payment_method;
+        $order->payment_status = 'pending';
+        $order->status = 'new';
+        $order->currency = 'idr';
+        $order->shipping_amount = 0;
+        $order->shipping_method = 'none';
+        $order->notes = 'Order dibuat oleh ' . auth()->user()->name;
+
+        $address = new Address();
+        $address->first_name = $this->first_name;
+        $address->last_name = $this->last_name;
+        $address->phone = $this->phone;
+        $address->alamat = $this->alamat;
+        $address->kabkota = $this->kabkota;
+        $address->provinsi = $this->provinsi;
+        $address->kode_pos = $this->kode_pos;
+
+        $order->save();
+        $address->order_id = $order->id;
+        $address->save();
+        $order->items()->createMany($cart_items);
+        CartManagement::clearCartItems();
+        return redirect('/success');
     }
-
-
 
     public function render()
     {
